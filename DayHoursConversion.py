b32b6e56b4eb87af729be18533739ed4be9dd4c8
Winnie Cai <79169487+sysadwcai@users.noreply.github.com>
2021-05-08 20:46:08 -0400

    Add files via upload

---
+++ DayHoursConversion.py
@@ -0,0 +1,30 @@
+def days_to_units(num_of_days, conversion_unit):
+    if conversion_unit == "hours":
+        return f"{num_of_days} days are {num_of_days * 24} hours"
+    elif conversion_unit == "minutes":
+        return f"{num_of_days} days are {num_of_days * 24 * 60} minutes"
+    else:
+        return "unsupported unit"
+
+def validate_and_execute():
+    try:
+        user_input_number = int(days_and_unit_dictionary["days"])
+        if user_input_number > 0:
+            calculated_value = days_to_units(user_input_number, days_and_unit_dictionary["unit"])
+            print(calculated_value)
+        elif user_input_number == 0:
+            print("You entered zero, please enter a positive number.")
+        else:
+            print("You entered a negative number.")
+    except ValueError:
+        print("Your input is not a valid number.")
+
+
+user_input = "" #initializing the user input
+while user_input != "exit":
+    user_input = input("Enter a number of days and conversion unit! \n")
+    days_and_unit = user_input.split(":")
+    print(days_and_unit)
+    days_and_unit_dictionary = {"days": days_and_unit[0], "unit": days_and_unit[1]}
+    print(days_and_unit_dictionary)
+    validate_and_execute()
